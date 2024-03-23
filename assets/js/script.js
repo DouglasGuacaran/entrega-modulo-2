fbe443c351af38bc3b8bd766259f75f1d27ed5a0
--global <--global>
2024-03-22 21:55:59 -0300

    Primer Commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  // Al hacer clic en una pestaña, activarla y mostrar el contenido correspondiente
+    $('.nav-link').click(function() {
+    $('.nav-link').removeClass('active');
+    $(this).addClass('active');
+    var tabId = $(this).attr('href');
+    $('.tab-pane').removeClass('show active');
+    $(tabId).addClass('show active');
+  });
+  
+});
